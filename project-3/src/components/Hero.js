b6249a6f4f4268c4dbbcaf548bc85bc0403eb769
Emilio Conde <condeemilio891@gmail.com>
2021-01-05 14:39:16 -0600

    picture done video in the process of being created. Can not get video to float right

--- project-3/src/components/Hero.js
+++ project-3/src/components/Hero.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import {Button} from './Button'
 import './Hero.css'
+import Iframe from 'react-iframe'
 function HeroSection (){
     return(
         <div className='hero-container'>
-            {/* <video src=".videos/video-2.mp4" autoPlay loop muted/> */}
+            {/* <Iframe url="https://giphy.com/embed/SiIcgS17qLCTYdUafU"
+        width="10%"
+        height="100%"
+        right= "100px"
+        id="myId"
+        className="video"
+        display="initial"
+        float="right"
+        z-index="-1"
+        position="absolute"/> */}
             <h1>RuneScape</h1>
             <p>Adventure Awaits</p>
             <div className="hero-btns">
