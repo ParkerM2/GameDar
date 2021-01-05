100c89fbbb88d33070281271a2d388f01d26eb1b
Emilio Conde <condeemilio891@gmail.com>
2021-01-05 12:37:53 -0600

    page

---
+++ project-3/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+
+function Navbar(){
+    return (
+        <>
+<nav className="navbar">
+        <div className="navbar-container">
+
+            //link is like an anchor tab this is the logo of the navbar
+            <Link to="/" className="navbar-logo"></Link>
+
+        </div>
+
+    </nav>
+
+         </>
+    )
+}
+export default Navbar
