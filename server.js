6544903ee3a5853c16ff31af630a8ba20dcaf7d9
Chambers Mansour <chambersmansour97@gmail.com>
2021-01-14 04:53:39 -0600

    server static files with express server

--- server.js
+++ server.js
@@ -55,4 +55,12 @@ homePage.handleHelloWorld(app);
 
 let port = process.env.PORT || 8080
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname + '/client/build')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/client/build/index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Building a login system with NodeJS is running on port ${port}!`));
